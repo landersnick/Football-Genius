3ba8fc82febc84efbbeeca7d17e0c3eba09145a1
Nick Landers <landersnick@proton.me>
2025-07-20 15:47:26 -0500

    Add Linux server

--- routes/public.js
+++ routes/public.js
@@ -8,17 +8,18 @@ router.get('/', function(req, res, next) {
     res.sendFile(path.resolve('public/main.html') );
 });
 
-router.get('/play_off.html', function(req, res, next) {
-    res.sendFile(path.resolve('public/play_off.html') ); 
-});
 
-router.get('/play_def.html', function(req, res, next) {
-    res.sendFile(path.resolve('public/play_def.html') );
-});
 router.get('/*', function(req, res, next) {
     res.sendFile(path.resolve('public/' + req.url) );
 });
 
+// Not Needed for now 
+// router.get('/play_off.html', function(req, res, next) {
+//     res.sendFile(path.resolve('public/play_off.html') ); 
+// });
 
+// router.get('/play_def.html', function(req, res, next) {
+//     res.sendFile(path.resolve('public/play_def.html') );
+// });
 
 module.exports = router;
